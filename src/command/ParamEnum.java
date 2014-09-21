c1e746dc47b75107773f82ff9ed0db0e924928b7
xuanyi <xuanyi.custode@gmail.com>
2014-09-21 23:52:43 +0800

    Add params enum

---
+++ src/command/ParamEnum.java
@@ -0,0 +1,18 @@
+package command;
+
+public enum ParamEnum {
+	DATE (),
+	NOTE (),
+	LEVEL (),
+	TAG (),
+	TASK_NAME (),
+	START_DATE (),
+	END_DATE (),
+	TASK_NUMBER (),
+	ORDER_BY (),
+	DESCRIPTION (),
+	EITHER (),
+	
+	ParamEnum() {	
+	}
+}
