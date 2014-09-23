4c7b40b0c30e61512483609379430e9c216b22e6
choyukchow <choyukchow@gmail.com>
2014-09-23 16:41:33 +0800

    add in starage class

---
+++ src/storage/Storage.java
@@ -0,0 +1,16 @@
+package storage;
+
+import models.Task;
+import models.exceptions.TaskNotFoundException;
+
+/**
+ *
+ * @author chuyu 
+ * This interface supports all the storage functionality
+ */
+public interface Storage implements IStorage{
+    public Storage(){
+        
+    }
+
+}
