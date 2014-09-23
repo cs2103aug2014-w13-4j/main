9f220f982be4365822e187eb471e8956c9915fb1
Pan Peining <peining_pan@hotmail.com>
2014-09-23 23:32:55 +0800

    Added in cases for all the command types available

--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -18,12 +18,29 @@ public class Logic implements ILogic {
 		case ADD:
 			Task task = new Task();
 			String taskName = null;
+			//TODO: get task name
 			//String taskName = params.get(ParamEnum.TASK_NAME);
 			Calendar dateEnd = Calendar.getInstance();
 			//TODO: Set date
 			dateEnd.set(2012, 10);
 			task.setName(taskName);
 			task.setDateEnd(dateEnd);
+			return null;
+		case DELETE:
+			return null;
+		case UPDATE:
+			return null;
+		case UNDO:
+			return null;
+		case SELECT:
+			return null;
+		case DISPLAY: 
+			return null;
+		case DONE:
+			return null;
+		case TAG:
+			return null;
+		case LEVEL:
 			return null;
 		default:
 			return null;
