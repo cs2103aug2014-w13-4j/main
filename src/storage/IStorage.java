0a387ce267ba394aff6595ae357ee6d0a1bf1b18
choyukchow <choyukchow@gmail.com>
2014-09-24 13:36:53 +0800

    add in throw exceptions

--- src/storage/IStorage.java
+++ src/storage/IStorage.java
@@ -8,12 +8,17 @@ import models.exceptions.TaskNotFoundException;
  * @author chuyu 
  * This interface supports all the storage functionality
  */
-public interface Storage {
+public interface IStorage {
+    // Get an instance of storage class
+    // The storage class follows the singleton pattern
+    // to ensure that there has exactly one instance the storage class
+    Storage getInstance() throws IOException;
+
     // Add/Update a task to file
-    void writeTaskToFile(Task task);
+    void writeTaskToFile(Task task) throws IOException;
 
     // delete a task to file
-    void deleteTaskFromFile(int taskID) throws TaskNotFoundException;
+    void deleteTaskFromFile(int taskID) throws TaskNotFoundException, IOException;
 
     // Get a task by task ID
     Task getTasks(int taskID) throws TaskNotFoundException;
