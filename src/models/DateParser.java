73d84fc72f58ba13e86830fd5a79cd1c8802fefd
Pan Peining <peining_pan@hotmail.com>
2014-09-26 22:14:55 +0800

    Added in comment, and extracted a constant out

--- src/models/DateParser.java
+++ src/models/DateParser.java
@@ -7,45 +7,58 @@ import java.util.Date;
 import java.util.Locale;
 
 public class DateParser {
-	
-	private static String[] validDateFormats = {"dd-MM-yyyy HH:mm a", "dd-MM-yyyy", "dd.MM.yyyy"};
-	
+
+	private static String[] validDateFormats = { "dd-MM-yyyy HH:mm a",
+			"dd-MM-yyyy", "dd.MM.yyyy" };
+
+	/**
+	 * Reads a date in the string format, and returns its corresponding calendar
+	 * representation
+	 * 
+	 * @param dateString
+	 *            : date in string format
+	 * @return the calendar object representing the date
+	 */
 	public static Calendar parseDate(String dateString) {
 		for (int i = 0; i < validDateFormats.length; i++) {
 			try {
-				SimpleDateFormat formatter = new SimpleDateFormat(validDateFormats[i]);
+				SimpleDateFormat formatter = new SimpleDateFormat(
+						validDateFormats[i]);
 				Date date = formatter.parse(dateString);
 				Calendar calendar = Calendar.getInstance();
 				calendar.setTime(date);
 				return calendar;
 			} catch (ParseException e) {
-				
+
 			}
 		}
 		return null;
 	}
-	
+
 	private static void testDate(String dateString) {
 		Calendar myCalendar;
 		if ((myCalendar = parseDate(dateString)) == null) {
 			System.out.println("Invalid Date Format!");
 		} else {
 			int day = myCalendar.get(Calendar.DAY_OF_MONTH);
-			String month = myCalendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault());
+			String month = myCalendar.getDisplayName(Calendar.MONTH,
+					Calendar.SHORT, Locale.getDefault());
 			int year = myCalendar.get(Calendar.YEAR);
 			int hour = myCalendar.get(Calendar.HOUR_OF_DAY);
 			int minute = myCalendar.get(Calendar.MINUTE);
-			System.out.println(day + " " + month + " " + year +" " + hour + " "+  minute);
+			String time = String.format("%02d:%02d", hour, minute);
+			System.out.println(day + " " + month + " " + year + " " + time);
 		}
 	}
 
-	public static void main (String[] args) throws ParseException {
+	public static void main(String[] args) throws ParseException {
 		testDate("02-10-1999 11:00 am");
 		testDate("02-10-1999");
 		testDate("2.10.1999");
 		testDate("2-10-1999");
 		testDate("10-1-2001");
-		testDate("2-10-2010 23:59");
+		testDate("2-10-2010 23:59"); // Date Format for time not fully
+										// implemented yet
 	}
 
 }
