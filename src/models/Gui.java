2b9560328d96ab52b3693628756af17d8b9f6803
Sue Zheng Hao <szhlibrary@gmail.com>
2014-09-29 21:26:15 +0800

    Fixed some errors

--- src/models/Gui.java
+++ src/models/Gui.java
@@ -1,12 +1,14 @@
 package models;
 import gui.iGui;
 import command.CommandParser;
-import logic.Logic;
+import command.Command;
+//import logic.Logic;
 
 public class Gui implements iGui {
 
 	public void main(String[] args){
-		Command userInput = parseCommand(args);
+		CommandParser commandParser = new CommandParser();
+		Command userInput = commandParser.parseCommand(args[0]);
 		Feedback executionResult = executeCommand(userInput);
 		// TODO: Link result to GUI
 	}
