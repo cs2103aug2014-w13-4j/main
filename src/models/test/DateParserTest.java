7e31d911f1086b6fe3add711f03672dbf8e04cce
Pan Peining <peining_pan@hotmail.com>
2014-09-30 20:24:24 +0800

    Modified DateParser to convert string to calendar object, and wrote a test for it

---
+++ src/models/test/DateParserTest.java
@@ -0,0 +1,28 @@
+package models.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import models.DateParser;
+
+import org.junit.Test;
+
+public class DateParserTest {
+
+	@Test
+	public final void test() {
+		Calendar date = DateParser.parseString("20-10-1999 23:00");
+		//System.out.println(date.get(Calendar.HOUR));
+		//Calendar expectedDate = Calendar.getInstance();
+		//month value is 0 based
+		//expectedDate.set(1999, 9, 20, 23, 00);
+		//assertEquals("Dates are equal", expectedDate, date);
+		String dateString = DateParser.parseCalendar(date);
+		String expectedDateString = "20-10-1999 23:00";
+		assertEquals("Strings are equal", expectedDateString, dateString);
+		Calendar newDate = DateParser.parseString(dateString);
+		assertEquals("Dates are still equal", newDate, date);
+	}
+
+}
