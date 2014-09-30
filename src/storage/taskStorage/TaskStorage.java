8dfaef54b04c20658867391d679efc2401e5e6a5
choyukchow <choyukchow@gmail.com>
2014-09-30 18:58:00 +0800

    changes in task to string and string to task method

--- src/storage/taskStorage/TaskStorage.java
+++ src/storage/taskStorage/TaskStorage.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,18 +32,18 @@ public class TaskStorage {
     private static final int ID_FOR_FIRST_TASK = 0;
 
     private static final String MESSAGE_SEPARATOR = "\tL@L";
-    private static final String MESSAGE_ID = "Task ID: "  + MESSAGE_SEPARATOR;
-    private static final String MESSAGE_NAME = "Name: " + MESSAGE_SEPARATOR;
-    private static final String MESSAGE_DATE_DUE = "Due date: " + MESSAGE_SEPARATOR;
-    private static final String MESSAGE_DATE_START = "Start date: " + MESSAGE_SEPARATOR;
-    private static final String MESSAGE_DATE_END = "End date: " + MESSAGE_SEPARATOR;
-    private static final String MESSAGE_PRIORITY_LEVEL = "Priority level: " + MESSAGE_SEPARATOR;
-    private static final String MESSAGE_NOTE = "Note: " + MESSAGE_SEPARATOR;
+    private static final String MESSAGE_ID = "Task ID: ";
+    private static final String MESSAGE_NAME = "Name: ";
+    private static final String MESSAGE_DATE_DUE = "Due date: ";
+    private static final String MESSAGE_DATE_START = "Start date: ";
+    private static final String MESSAGE_DATE_END = "End date: ";
+    private static final String MESSAGE_PRIORITY_LEVEL = "Priority level: ";
+    private static final String MESSAGE_NOTE = "Note: ";
     private static final String MESSAGE_TAGS = "Tags: ";
     private static final String MESSAGE_PARENT_TASKS = "Parent tasks: ";
     private static final String MESSAGE_CHILD_TASKS = "Child tasks: ";
     private static final String MESSAGE_CONDITIONAL_TASKS = "Conditional tasks: ";
-    private static final String MESSAGE_IS_DELETED = "Is deleted :";
+    private static final String MESSAGE_IS_DELETED = "Is deleted: ";
     private static final String MESSAGE_IS_COMFIRMED = "Is comfirmed: ";
 
     private static final int ID_ATTRIBUTE = 1;
@@ -55,6 +56,8 @@ public class TaskStorage {
     private static final int IS_DELETED_ATTRIBUTE = 15;
     private static final int IS_COMFIRMED_ATTRIBUTE = 17;
     private static final int TAGS_ATTRIBUTE = 19;
+
+    private static final int RESULT_THRESHOLD = -1;
 
      /**
      * constructor``
@@ -78,15 +81,112 @@ public class TaskStorage {
         }   
     }
 
+    // convert int task property to string
+    private String taskPropertyToString(Integer intProperty) {
+        if (intProperty == null) {
+            return "";
+        } else {
+            return intProperty.toString();
+        }
+    }
+
+    // convert string task property to string
+    private String taskPropertyToString(String strProperty) {
+        return strProperty;
+    }
+
+    // convert calendar task property to string
+    private String taskPropertyToString(Calendar calendarProperty) {
+        if (calendarProperty == null) {
+            return "";
+        } else {
+            return calendarProperty.toString();
+        }
+    }
+
+    // convert boolean task property to string
+    private String taskPropertyToString(Boolean booleanProperty) {
+        if (booleanProperty == null) {
+            return "";
+        } else {
+            return booleanProperty.toString();
+        }
+    }
+
+    // convert string array list task property to string
+    private String taskPropertyToString(ArrayList<String> stringArrayListProperty) {
+        if (stringArrayListProperty == null) {
+            return "";
+        } else {
+            return arrayListToString(stringArrayListProperty);
+        }
+    }
+
+    // convert integer array list task property to string
+    private String taskIntPropertyToString(ArrayList<Integer> intArrayListProperty) {
+        if (intArrayListProperty == null) {
+            return "";
+        } else {
+            ArrayList<String> stringArrayProperty = new ArrayList<String>();
+            for (int intProperty : intArrayListProperty) {
+                stringArrayProperty.add(taskPropertyToString(intProperty));
+            }
+            return taskPropertyToString(stringArrayProperty);
+        }
+    }
+
+    private String arrayListToString(ArrayList<String> stringArray) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String str : stringArray) {
+            // System.out.println(str + "LOL");
+            stringBuilder.append(str + MESSAGE_SEPARATOR);
+        }
+        return stringBuilder.toString();
+    }
+
     private String TaskToString(Task task) {
-    	String taskID = Integer.toString(task.getId()) + MESSAGE_SEPARATOR;
-        String taskName = task.getName() + MESSAGE_SEPARATOR;
-        String taskDateDue = task.getDateDue().toString() + MESSAGE_SEPARATOR;
-        String taskDateStart = task.getDateStart().toString() + MESSAGE_SEPARATOR;
-        String taskDateEnd = task.getDateEnd().toString() + MESSAGE_SEPARATOR;
-        String taskPriorityLevel = Integer.toString(task.getPriorityLevelInteger()) + MESSAGE_SEPARATOR;
-        String taskNote = task.getNote() + MESSAGE_SEPARATOR;
-        String taskIsDeleted = Boolean.toString(task.isDeleted()) + MESSAGE_SEPARATOR;
+        ArrayList<String> taskStringArrayList = new ArrayList<String>();
+        taskStringArrayList.add(MESSAGE_ID);
+        taskStringArrayList.add(taskPropertyToString(task.getId()));
+        taskStringArrayList.add(MESSAGE_NAME);
+        taskStringArrayList.add(taskPropertyToString(task.getName()));
+        taskStringArrayList.add(MESSAGE_DATE_DUE);
+        taskStringArrayList.add(taskPropertyToString(task.getDateDue()));
+        taskStringArrayList.add(MESSAGE_DATE_START);
+        taskStringArrayList.add(taskPropertyToString(task.getDateStart()));
+        taskStringArrayList.add(MESSAGE_DATE_END);
+        taskStringArrayList.add(taskPropertyToString(task.getDateEnd()));
+        taskStringArrayList.add(MESSAGE_PRIORITY_LEVEL);
+        taskStringArrayList.add(taskPropertyToString(task.getPriorityLevelInteger()));
+        taskStringArrayList.add(MESSAGE_NOTE);
+        taskStringArrayList.add(taskPropertyToString(task.getNote()));
+        taskStringArrayList.add(MESSAGE_IS_DELETED);
+        taskStringArrayList.add(taskPropertyToString(task.isDeleted()));
+        taskStringArrayList.add(MESSAGE_IS_COMFIRMED);
+        taskStringArrayList.add(taskPropertyToString(task.isConfirmed()));
+        taskStringArrayList.add(MESSAGE_TAGS);
+        taskStringArrayList.add(taskPropertyToString(task.getTags()));
+        taskStringArrayList.add(MESSAGE_PARENT_TASKS);
+        taskStringArrayList.add(taskIntPropertyToString(task.getParentTasks()));
+        taskStringArrayList.add(MESSAGE_CHILD_TASKS);
+        taskStringArrayList.add(taskIntPropertyToString(task.getChildTasks()));
+        taskStringArrayList.add(MESSAGE_CONDITIONAL_TASKS);
+        taskStringArrayList.add(taskIntPropertyToString(task.getConditionalTasks()));
+
+        String taskString = arrayListToString(taskStringArrayList);
+        return taskString;
+
+
+        /*
+        String[taskStringArray] = getPropertyArray(task);
+    	String taskID = Integer.toString(task.getId());
+        String taskName = task.getName();
+        String taskDateDue = task.getDateDue().toString();
+        String taskDateStart = task.getDateStart().toString();
+        String taskDateEnd = task.getDateEnd().toString();
+        String taskPriorityLevel = Integer.toString(task.getPriorityLevelInteger());
+        String taskNote = task.getNote();
+        String taskIsDeleted = Boolean.toString(task.isDeleted());
         String taskIsConfirmed = Boolean.toString(task.isConfirmed());
         String[] taskStringArray = new String[]{MESSAGE_ID, taskID, MESSAGE_NAME, taskName,
             MESSAGE_DATE_DUE, taskDateDue, MESSAGE_DATE_START, taskDateStart, MESSAGE_DATE_END,
@@ -110,6 +210,21 @@ public class TaskStorage {
             taskString = taskString + MESSAGE_SEPARATOR + conditionalID;
         }
         return taskString;
+        */
+    }
+
+    private Calendar stringtoTaskProperty(String propertyString) throws ParseException {
+        if (propertyString.equals("")) {
+            // System.out.println("null property");
+            return null;            
+        } else {
+            Calendar date = Calendar.getInstance();
+
+            // ??need to confirm date format
+            DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss");
+            date.setTime(dateFormat.parse(propertyString));
+            return date;
+        }
     }
 
     private Task stringToTask(String taskString) throws FileFormatNotSupportedException {
@@ -117,25 +232,26 @@ public class TaskStorage {
         String tagStored;
         int taskStored;
         Task task = new Task();
-        // ??need to confirm date format
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Calendar taskDateDue = Calendar.getInstance();
-        Calendar taskDateStart = Calendar.getInstance();
-        Calendar taskDateEnd = Calendar.getInstance();
 
         try{
-            String[] taskStringArray = taskString.split(MESSAGE_SEPARATOR);
+            // taskString  = taskString.replaceAll("[\n\r\\s]", "");
+            String[] taskStringArray = taskString.split(MESSAGE_SEPARATOR, RESULT_THRESHOLD);
             int taskID = Integer.valueOf(taskStringArray[ID_ATTRIBUTE]);
-            String taskName = taskStringArray[NAME_ATTRIBUTE];       
-            taskDateDue.setTime(dateFormat.parse(taskStringArray[DATE_DUE_ATTRIBUTE]));
-            taskDateStart.setTime(dateFormat.parse(taskStringArray[DATE_START_ATTRIBUTE]));
-            taskDateEnd.setTime(dateFormat.parse(taskStringArray[DATE_END_ATTRIBUTE]));
+            String taskName = taskStringArray[NAME_ATTRIBUTE];
+            Calendar taskDateDue = stringtoTaskProperty(taskStringArray[DATE_DUE_ATTRIBUTE]);
+            Calendar taskDateStart = stringtoTaskProperty(taskStringArray[DATE_START_ATTRIBUTE]);
+            Calendar taskDateEnd = stringtoTaskProperty(taskStringArray[DATE_END_ATTRIBUTE]);  
             PriorityLevelEnum taskPriorityLevel = PriorityLevelEnum.fromInteger(Integer.valueOf(taskStringArray[PRIORITY_LEVEL_ATTRIBUTE]));
             String taskNote = taskStringArray[NOTE_ATTRIBUTE];
             boolean taskIsDeleted = Boolean.valueOf(taskStringArray[IS_DELETED_ATTRIBUTE]);
             boolean taskIsConfirmed = Boolean.valueOf(taskStringArray[IS_COMFIRMED_ATTRIBUTE]);
             ArrayList<String> taskTags = new ArrayList<String>();
             arrayIndex = TAGS_ATTRIBUTE;
+            // need to refactor later
+            if (taskStringArray[arrayIndex].equals("")) {
+                taskTags = null;
+                arrayIndex ++;
+            }
             while (!taskStringArray[arrayIndex].equals(MESSAGE_PARENT_TASKS)) {
                 tagStored = taskStringArray[arrayIndex];
                 taskTags.add(tagStored);
@@ -143,6 +259,11 @@ public class TaskStorage {
             }
             ArrayList<Integer> taskParentTasks = new ArrayList<Integer>();
             arrayIndex ++;
+            // need to refactor later
+            if (taskStringArray[arrayIndex].equals("")) {
+                taskParentTasks = null;
+                arrayIndex ++;
+            }
             while (!taskStringArray[arrayIndex].equals(MESSAGE_CHILD_TASKS)) {
                 taskStored = Integer.valueOf(taskStringArray[arrayIndex]);
                 taskParentTasks.add(taskStored);
@@ -150,6 +271,11 @@ public class TaskStorage {
             }
             ArrayList<Integer> taskChildTasks = new ArrayList<Integer>();
             arrayIndex ++;
+            // need to refactor later
+            if (taskStringArray[arrayIndex].equals("")) {
+                taskChildTasks = null;
+                arrayIndex ++;
+            }
             while (!taskStringArray[arrayIndex].equals(MESSAGE_CONDITIONAL_TASKS)) {
                 taskStored = Integer.valueOf(taskStringArray[arrayIndex]);
                 taskChildTasks.add(taskStored);
@@ -157,12 +283,17 @@ public class TaskStorage {
             }
             ArrayList<Integer> taskConditionalTasks = new ArrayList<Integer>();
             arrayIndex ++;
-            while (arrayIndex <= (taskStringArray.length - 1)) {
+            // need to refactor later
+            if (taskStringArray[arrayIndex].equals("")) {
+                taskConditionalTasks = null;
+                arrayIndex ++;
+            }
+            while (arrayIndex <= (taskStringArray.length - 2)) {
                 taskStored = Integer.valueOf(taskStringArray[arrayIndex]);
                 taskConditionalTasks.add(taskStored);
                 arrayIndex ++;
             }
-
+            
             task.setId(taskID);
             task.setName(taskName);
             task.setDateDue(taskDateDue);
@@ -232,7 +363,7 @@ public class TaskStorage {
         try {
             String taskString = TaskToString(task);
             bufferedWriter = new BufferedWriter(new FileWriter(dataFile, true));
-            bufferedWriter.write(taskString);
+            bufferedWriter.write(taskString + "\r\n");
             bufferedWriter.close();
         } finally {
             
