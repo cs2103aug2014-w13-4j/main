d523672883f22026cdf12cd3f8bdda43178a4706
Sue Zheng Hao <szhlibrary@gmail.com>
2014-09-30 11:21:02 +0800

    Preliminary UI; only allows parsing of user command via parseCommand; does not do anything with it yet.

---
+++ src/main/MainController.java
@@ -0,0 +1,30 @@
+package main;
+
+import command.Command;
+import command.CommandParser;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * Controller for the main GUI window.
+ * @author szhlibrary
+ */
+public class MainController {
+	public Label taskNameLabel;
+	public Label dueDateLabel;
+	public TextField userInputField;
+
+	public void handleUserInput() {
+		// TODO: handle user command
+		CommandParser commandParser = new CommandParser();
+		String userInput = userInputField.getText();
+		if (validateUserInput(userInput)){
+			Command userCommand = commandParser.parseCommand(userInput);
+			userInputField.clear();
+		}
+	}
+
+	private boolean validateUserInput(String userInput){
+		return (userInput != null && !userInput.isEmpty()) ? true : false;
+	}
+}
