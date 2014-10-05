6ea2c8cd21111695f8a402b9745850db8d1242c7
Pan Peining <peining_pan@hotmail.com>
2014-10-05 13:36:38 +0800

    Separated task modifier into a separate class

--- src/interfaces/ILogic.java
+++ src/interfaces/ILogic.java
@@ -1,9 +1,14 @@
 package interfaces;
 
+import java.io.IOException;
+
 import command.Command;
+import exceptions.InvalidDateFormatException;
+import exceptions.InvalidInputException;
+import exceptions.TaskNotFoundException;
 import models.Feedback;
 
 public interface ILogic {
-	public Feedback executeCommand(Command command);
+	public Feedback executeCommand(Command command) throws TaskNotFoundException, IOException, InvalidDateFormatException, InvalidInputException;
 
 }
