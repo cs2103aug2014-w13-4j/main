fad8f1d52642a0c7b193b54dd5bf281554c42a34
Sue Zheng Hao <szhlibrary@gmail.com>
2014-10-05 23:11:09 +0800

    Increased the size of the display window to 1024x768

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
 		primaryStage.setTitle("Awesome Task Manager");
-		primaryStage.setScene(new Scene(root, 800, 600));
+		primaryStage.setScene(new Scene(root, 1024, 768));
 		primaryStage.show();
 	}
 
