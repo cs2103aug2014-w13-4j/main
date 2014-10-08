11873c910c2f22821f08b7abbcd913b136870aa8
choyukchow <choyukchow@gmail.com>
2014-10-08 13:11:08 +0800

    add in test for conditional task

--- src/tests/TaskStorageTest.java
+++ src/tests/TaskStorageTest.java
@@ -7,7 +7,9 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
+import models.DateParser;
 import models.PriorityLevelEnum;
+import models.StartDueDatePair;
 import models.Task;
 import command.ParamEnum;
 
@@ -19,13 +21,14 @@ import storage.taskStorage.TaskStorage;
 public class TaskStorageTest {
     private static final int ID_FOR_NEW_TASK = -1;
 
-    private Task createTaskForTest(int id, String name, int priorityLevel, String note, boolean isDeleted, boolean isConfirmed) {
+    private Task createTaskForTest(int id, String name, int priorityLevel, String note, boolean isDeleted, ArrayList<StartDueDatePair> datePair) {
         Task task = new Task();
         task.setId(id);
         task.setName(name);
         task.setPriorityLevel(PriorityLevelEnum.fromInteger(priorityLevel));
         task.setNote(note);
         task.setDeleted(isDeleted);
+        task.setConditionalDates(datePair);
         return task;
     }
 
@@ -40,10 +43,16 @@ public class TaskStorageTest {
             */
 
 			TaskStorage taskStorage = new TaskStorage("taskStorage.data");
-            Task task = createTaskForTest(ID_FOR_NEW_TASK, "Write Report", 1, "Do eat apple when you are writing report.", false, false);
+            ArrayList<StartDueDatePair> datePairArrayList = new ArrayList<StartDueDatePair>();
+            StartDueDatePair datePairA = new StartDueDatePair(DateParser.parseString("23.10.2010"), DateParser.parseString("24.10.2010"));
+            StartDueDatePair datePairB = new StartDueDatePair(DateParser.parseString("25.10.2010"), DateParser.parseString("27.10.2010"));
+            datePairArrayList.add(datePairA);
+            datePairArrayList.add(datePairB);
+
+            Task task = createTaskForTest(ID_FOR_NEW_TASK, "Write Report", 1, "Do eat apple when you are writing report.", false, datePairArrayList);
             taskStorage.writeTaskToFile(task);
             assertEquals(task, taskStorage.getTask(taskStorage.getAllTasks().size()-1));
-            task = createTaskForTest(0, "Read Report", 1, "Do eat apple when you are writing report.", false, false);
+            task = createTaskForTest(0, "Read Report", 1, "Do eat apple when you are writing report.", false, null);
             taskStorage.writeTaskToFile(task);
             assertEquals(task, taskStorage.getTask(0));
 		} catch (Exception e) {
