124f59688f749bbf3cbc7d0e68792462ca596c30
xuanyi <xuanyi.custode@gmail.com>
2014-10-21 19:59:03 +0800

    Added more functions for interal search

--- src/models/IntervalSearch.java
+++ src/models/IntervalSearch.java
@@ -2,12 +2,14 @@ package models;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 
-
 public class IntervalSearch {
+    
+    private static long END_OF_DATE = Long.MAX_VALUE;
 
-    public static class DateRange implements Comparable<DateRange> {
+    public static class DateRange {
         public long startDate;
         public long endDate;
 
@@ -62,27 +64,29 @@ public class IntervalSearch {
         }
         return results;
     }
-    
+
     public boolean isValid(Calendar start, Calendar end) {
         ArrayList<DateRange> results = searchInterval(start, end);
         return results.isEmpty();
     }
-    
-    public ArrayList<Integer> getTasksWithinInterval(Calendar start, Calendar end) {
+
+    public ArrayList<Integer> getTasksWithinInterval(Calendar start,
+            Calendar end) {
         ArrayList<Integer> results = new ArrayList<Integer>();
         ArrayList<DateRange> searchResults = searchInterval(start, end);
-        
-        for (DateRange r: searchResults) {
+
+        for (DateRange r : searchResults) {
             results.add(map.get(r));
         }
-        
+
         return results;
     }
-    
-    public HashMap<Calendar, Calendar> getOccupiedIntervals(Calendar start, Calendar end) {
+
+    public HashMap<Calendar, Calendar> getOccupiedIntervals(Calendar start,
+            Calendar end) {
         HashMap<Calendar, Calendar> results = new HashMap<Calendar, Calendar>();
         ArrayList<DateRange> searchResults = searchInterval(start, end);
-        for (DateRange range: searchResults) {
+        for (DateRange range : searchResults) {
             long startTime = range.getStartDate();
             long endTime = range.getEndDate();
             Calendar startCalendar = Calendar.getInstance();
@@ -94,27 +98,44 @@ public class IntervalSearch {
         return results;
     }
 
-    public boolean add(Calendar start, Calendar end, int id) {
+    public void add(Calendar start, Calendar end, int id) {
         assert start.getTimeInMillis() >= end.getTimeInMillis();
-        
-        if (isValid(start, end)) {
-            DateRange range = new DateRange(start, end);
-            map.put(range, id);
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    public boolean remove(Calendar start, Calendar end, int id) {
+
         DateRange range = new DateRange(start, end);
-        
-        if(map.containsKey(range)) {
+        map.put(range, id);
+    }
+
+    public void remove(Calendar start, Calendar end) {
+        DateRange range = new DateRange(start, end);
+
+        if (map.containsKey(range)) {
             map.remove(range);
-            return true;
-        } else {
-            return false;
         }
     }
 
+    public void update(Calendar oldStart, Calendar oldEnd, Calendar newStart,
+            Calendar newEnd, int id) {
+        remove(oldStart, oldEnd);
+        add(newStart, newEnd, id);
+    }
+
+    public int get(Calendar start, Calendar end) {
+        DateRange range = new DateRange(start, end);
+       
+        if (map.containsKey(range)) {
+            return map.get(range);
+        } else {
+            return -1;
+        }
+    }
+    
+    public ArrayList<Integer> getTasksFrom(Calendar start) {
+        Calendar end = GregorianCalendar.getInstance();
+        end.setTimeInMillis(END_OF_DATE);
+        return getTasksWithinInterval(start, end);
+    }
+
+    public int size() {
+        return map.size();
+    }
 }
