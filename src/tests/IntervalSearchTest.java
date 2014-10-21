a0e011a734fc229e3fa02e2851982544abcaa7a4
xuanyi <xuanyi.custode@gmail.com>
2014-10-21 20:01:38 +0800

    Added more test for interval search

--- src/tests/IntervalSearchTest.java
+++ src/tests/IntervalSearchTest.java
@@ -19,17 +19,17 @@ public class IntervalSearchTest {
     @Before
     public void initializeTest() {
         tr = new IntervalSearch();
-        
-        Calendar start = new GregorianCalendar(2014,11,12);
-        Calendar end = new GregorianCalendar(2014,11,13);
+
+        Calendar start = new GregorianCalendar(2014, 11, 12);
+        Calendar end = new GregorianCalendar(2014, 11, 13);
         tr.add(start, end, 3);
-        
-        Calendar secondStart = new GregorianCalendar(2014,11,15);
-        Calendar secondEnd = new GregorianCalendar(2014,11,18);
+
+        Calendar secondStart = new GregorianCalendar(2014, 11, 15);
+        Calendar secondEnd = new GregorianCalendar(2014, 11, 18);
         tr.add(secondStart, secondEnd, 4);
-        
-        Calendar thirdStart = new GregorianCalendar(2014,8,10);
-        Calendar thirdEnd = new GregorianCalendar(2014,9,12);
+
+        Calendar thirdStart = new GregorianCalendar(2014, 8, 10);
+        Calendar thirdEnd = new GregorianCalendar(2014, 9, 12);
         tr.add(thirdStart, thirdEnd, 5);
     }
 
@@ -37,45 +37,74 @@ public class IntervalSearchTest {
     public void testAddDate() {
         int currentSize = tr.size();
 
-        Calendar newStart = new GregorianCalendar(2014,10,3);
-        Calendar newEnd = new GregorianCalendar(2014,10,4);
+        Calendar newStart = new GregorianCalendar(2014, 10, 3);
+        Calendar newEnd = new GregorianCalendar(2014, 10, 4);
         tr.add(newStart, newEnd, 5);
         int newSize = tr.size();
 
-        assertTrue((newSize - currentSize) == 1);
+        assertEquals(1, (newSize - currentSize));
     }
 
     @Test
     public void testGetInterval() {
-        Calendar searchStart = new GregorianCalendar(2014,11,1);
-        Calendar searchEnd = new GregorianCalendar(2014,11,30);
+        Calendar searchStart = new GregorianCalendar(2014, 11, 1);
+        Calendar searchEnd = new GregorianCalendar(2014, 11, 30);
 
         HashMap<Calendar, Calendar> results = tr.getOccupiedIntervals(
                 searchStart, searchEnd);
         assertTrue(results.size() == 2);
 
-        Calendar testStart = new GregorianCalendar(2014,11,15);
-        Calendar testEnd = new GregorianCalendar(2014,11,18);
-        
-        assertEquals(results.get(testStart).getTimeInMillis(),testEnd.getTimeInMillis());
-        
-        Calendar testStart2 = new GregorianCalendar(2014,11,12);
-        Calendar testEnd2 = new GregorianCalendar(2014,11,13);
-        
-        assertEquals(results.get(testStart2).getTimeInMillis(),testEnd2.getTimeInMillis());
+        Calendar testStart = new GregorianCalendar(2014, 11, 15);
+        Calendar testEnd = new GregorianCalendar(2014, 11, 18);
+
+        assertEquals(testEnd.getTimeInMillis(), results.get(testStart)
+                .getTimeInMillis());
+
+        Calendar testStart2 = new GregorianCalendar(2014, 11, 12);
+        Calendar testEnd2 = new GregorianCalendar(2014, 11, 13);
+
+        assertEquals(testEnd2.getTimeInMillis(), results.get(testStart2)
+                .getTimeInMillis());
     }
-    
+
     @Test
     public void testGetIntervalId() {
-        Calendar searchStart = new GregorianCalendar(2014,11,1);
-        Calendar searchEnd = new GregorianCalendar(2014,11,30);
+        Calendar searchStart = new GregorianCalendar(2014, 11, 1);
+        Calendar searchEnd = new GregorianCalendar(2014, 11, 30);
 
-        ArrayList<Integer> results = tr.getTasksWithinInterval(searchStart, searchEnd);
+        ArrayList<Integer> results = tr.getTasksWithinInterval(searchStart,
+                searchEnd);
         assertTrue(results.size() == 2);
-        
+
         assertFalse(results.contains(5));
         assertTrue(results.contains(4));
         assertTrue(results.contains(3));
+    }
+
+    @Test
+    public void testGetIdFromDate() {
+        Calendar searchStart = new GregorianCalendar(2014, 11, 1);
+
+        ArrayList<Integer> results = tr.getTasksFrom(searchStart);
+        assertTrue(results.size() == 2);
+
+        assertFalse(results.contains(5));
+        assertTrue(results.contains(4));
+        assertTrue(results.contains(3));
+    }
+
+    @Test
+    public void testUpdate() {
+        Calendar oldStart = new GregorianCalendar(2014, 11, 12);
+        Calendar oldEnd = new GregorianCalendar(2014, 11, 13);
+
+        Calendar newStart = new GregorianCalendar(2014, 11, 13);
+        Calendar newEnd = new GregorianCalendar(2014, 11, 14);
+
+        tr.update(oldStart, oldEnd, newStart, newEnd, 3);
+        assertEquals(-1, tr.get(oldStart, oldEnd));
+        assertEquals(3, tr.get(newStart, newEnd));
+
     }
 
 }
