001e82183522af9664cb0f4e5f3534da56edd0b1
Sue Zheng Hao <szhlibrary@gmail.com>
2014-10-27 11:39:17 +0800

    Some controller skeletons.

---
+++ src/main/TaskDisplayViewController.java
@@ -0,0 +1,76 @@
+package main;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.Label;
+import models.DateParser;
+import models.StartDueDatePair;
+import models.Task;
+
+import java.util.ArrayList;
+
+/**
+ * @author szhlibrary
+ */
+public class TaskDisplayViewController {
+
+	public Label idLabel;
+	public Label taskNameLabel;
+	public Label dueDateLabel;
+	public Label startDateLabel;
+	public Label endDateLabel;
+	public Label priorityLevelLabel;
+	public Label noteLabel;
+	public Label conditionalDateLabel;
+
+	final StringProperty idLabelValue = new SimpleStringProperty("-");
+	final StringProperty taskNameLabelValue = new SimpleStringProperty("-");
+	final StringProperty dueDateLabelValue = new SimpleStringProperty("-");
+	final StringProperty startDateLabelValue = new SimpleStringProperty("-");
+	final StringProperty endDateLabelValue = new SimpleStringProperty("-");
+	final StringProperty priorityLevelLabelValue = new SimpleStringProperty("-");
+	final StringProperty noteLabelValue = new SimpleStringProperty("-");
+	final StringProperty conditionalDateLabelValue = new SimpleStringProperty("-");
+
+	private void initializeGuiLabelBindings() {
+		idLabel.textProperty().bind(idLabelValue);
+		taskNameLabel.textProperty().bind(taskNameLabelValue);
+		dueDateLabel.textProperty().bind(dueDateLabelValue);
+		startDateLabel.textProperty().bind(startDateLabelValue);
+		endDateLabel.textProperty().bind(endDateLabelValue);
+		priorityLevelLabel.textProperty().bind(priorityLevelLabelValue);
+		noteLabel.textProperty().bind(noteLabelValue);
+		conditionalDateLabel.textProperty().bind(conditionalDateLabelValue);
+	}
+
+	private void updateTaskPanel(Task taskToDisplay) {
+		setLabelValueInGui(idLabelValue, Integer.toString(taskToDisplay.getId()));
+		setLabelValueInGui(taskNameLabelValue, taskToDisplay.getName());
+		setLabelValueInGui(dueDateLabelValue, DateParser.parseCalendar(taskToDisplay.getDateDue()));
+		setLabelValueInGui(startDateLabelValue, DateParser.parseCalendar(taskToDisplay.getDateStart()));
+		setLabelValueInGui(endDateLabelValue, DateParser.parseCalendar(taskToDisplay.getDateEnd()));
+		setLabelValueInGui(priorityLevelLabelValue, (taskToDisplay.getPriorityLevel() == null ? null : taskToDisplay.getPriorityLevel().name()));
+		setLabelValueInGui(noteLabelValue, taskToDisplay.getNote());
+		updateTaskPanelForConditionalDates(taskToDisplay);
+	}
+
+	private void setLabelValueInGui(StringProperty labelValue, String value){
+		labelValue.setValue(value != null && !value.isEmpty() ? value : "-");
+	}
+
+	private void updateTaskPanelForConditionalDates(Task taskToDisplay){
+		ArrayList<StartDueDatePair> conditionalDateList = taskToDisplay.getConditionalDates();
+		String conditionalDates = "";
+		if (conditionalDateList != null) {
+			int dateId = 0;
+			for (StartDueDatePair conditionalDatePair : conditionalDateList) {
+				conditionalDates += dateId + ": " + DateParser.parseCalendar(conditionalDatePair.getStartDate())
+						+ " - " + DateParser.parseCalendar(conditionalDatePair.getDueDate())
+						+ "\n";
+				dateId++;
+			}
+			setLabelValueInGui(conditionalDateLabelValue, conditionalDates);
+		}
+	}
+}
+
---
+++ src/main/TaskListViewController.java
@@ -0,0 +1,34 @@
+package main;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import models.Feedback;
+import models.Task;
+
+import java.util.ArrayList;
+
+/**
+ * @author szhlibrary
+ */
+public class TaskListViewController {
+	@FXML
+	public TableView<Task> taskTableView;
+	public TableColumn dueDateTableColumn;
+
+	public void initialize(Feedback initialTasks){
+		ArrayList<Task> taskList = initialTasks.getTaskList();
+		ObservableList<Task> observableList = FXCollections.observableArrayList(taskList);
+		taskTableView.getItems().addAll(observableList);
+	}
+
+	private void updateTaskList(ArrayList<Task> taskList) {
+		assert(taskList != null && taskList.size() >= 0);
+
+		taskTableView.getItems().clear();
+		ObservableList<Task> observableList = FXCollections.observableArrayList(taskList);
+		taskTableView.getItems().addAll(observableList);
+	}
+}
