50e0b007fd71f0b5bfca2f86a3e58a5e4134cafb
Sue Zheng Hao <szhlibrary@gmail.com>
2014-10-28 11:45:36 +0800

    #79: Some dummy tests.

---
+++ src/main/RootLayoutControllerTest.java
@@ -0,0 +1,37 @@
+package main;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.*;
+
+public class RootLayoutControllerTest {
+
+	Class<RootLayoutController> rootLayoutControllerClass = RootLayoutController.class;
+	Method validateUserInput = rootLayoutControllerClass.
+			getDeclaredMethod("validateUserInput", String.class);
+
+	public RootLayoutControllerTest() throws NoSuchMethodException {
+	}
+
+	@Before
+	public void setFunctionsAccessible(){
+		//setFocusToUserInputField.setAccessible(true);
+		validateUserInput.setAccessible(true);
+	}
+
+	public void testExecuteCommand() throws Exception {
+
+	}
+
+	@Test
+	public void testValidateUserInput() throws Exception {
+		RootLayoutController controller = new RootLayoutController();
+		assertEquals("Empty input did not validate to false!", false,
+				validateUserInput.invoke(controller, ""));
+		assertEquals("Valid input did not validate to true!", true,
+				validateUserInput.invoke(controller, "test command"));
+	}
+}
---
+++ src/main/UserInputViewControllerTest.java
@@ -0,0 +1,28 @@
+package main;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.*;
+
+public class UserInputViewControllerTest {
+
+	Class<UserInputViewController> UserInputViewControllerClass = UserInputViewController.class;
+
+	public UserInputViewControllerTest() throws NoSuchMethodException {
+	}
+
+	@Test
+	public void testHandleUserIncrementalInput() throws Exception {
+
+	}
+
+	@Test
+	public void testHandleUserInput() throws Exception {
+
+	}
+
+
+}
