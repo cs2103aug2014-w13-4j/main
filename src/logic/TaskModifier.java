7b66c79f9bda2a75c1148ffe1633c005acb327b8
Pan Peining <peining_pan@hotmail.com>
2014-10-31 19:33:34 +0800

    Clean up

--- src/logic/TaskModifier.java
+++ src/logic/TaskModifier.java
@@ -15,56 +15,6 @@ import exceptions.InvalidInputException;
 import exceptions.InvalidPriorityLevelException;
 
 public class TaskModifier {
-    private static final String INVALID_PRIORITY_LEVEL_MESSAGE = "%1$s is not a valid priority level.";
-    private static final String INVALID_CONDITIONAL_DATE_ID_MESSAGE = "The conditional date id is invalid.";
-    private static final int MIN_ID = 0;
-    private static final String INVALID_CONFIRMED_TASK_MESSAGE = "The task is already confirmed.";
-    private static final String INVALID_START_END_DATE_MESSAGE = "The start date should occur before the end date";
-
-    static void modifyTimedTask(Hashtable<ParamEnum, ArrayList<String>> param,
-            Task task) throws InvalidDateFormatException, InvalidInputException {
-        setNameFromCommand(param, task);
-        setTagsFromCommand(param, task);
-        setLevelFromCommand(param, task);
-        setNoteFromCommand(param, task);
-        setStartDateFromCommand(param, task);
-        setEndDateFromCommand(param, task);
-        checkStartDateIsBeforeEndDate(task.getDateStart(), task.getDateEnd());
-    }
-
-    static void modifyConditionalTask(
-            Hashtable<ParamEnum, ArrayList<String>> param, Task task)
-            throws InvalidDateFormatException, InvalidInputException {
-        setNameFromCommand(param, task);
-        setTagsFromCommand(param, task);
-        setLevelFromCommand(param, task);
-        setNoteFromCommand(param, task);
-        setConditionalDatesFromCommand(param, task);
-    }
-
-    static void modifyDeadlineTask(
-            Hashtable<ParamEnum, ArrayList<String>> param, Task task)
-            throws InvalidDateFormatException, InvalidInputException {
-        setNameFromCommand(param, task);
-        setTagsFromCommand(param, task);
-        setLevelFromCommand(param, task);
-        setNoteFromCommand(param, task);
-        setDueDateFromCommand(param, task);
-    }
-
-    static void modifyFloatingTask(
-            Hashtable<ParamEnum, ArrayList<String>> param, Task task)
-            throws InvalidDateFormatException, InvalidInputException {
-        setNameFromCommand(param, task);
-        setTagsFromCommand(param, task);
-        setLevelFromCommand(param, task);
-        setNoteFromCommand(param, task);
-    }
-
-    static void deleteTask(Task task) {
-        task.setDeleted(true);
-    }
-
     static void completeTask(Hashtable<ParamEnum, ArrayList<String>> param,
             Task task) throws InvalidDateFormatException, InvalidInputException {
         if (param.containsKey(ParamEnum.DATE)) {
@@ -94,9 +44,48 @@ public class TaskModifier {
         }
     }
 
-    static void undeleteTask(Task task) {
-        assert (task.isDeleted());
-        task.setDeleted(false);
+    static void deleteTask(Task task) {
+        task.setDeleted(true);
+    }
+
+    static void modifyConditionalTask(
+            Hashtable<ParamEnum, ArrayList<String>> param, Task task)
+                    throws InvalidDateFormatException, InvalidInputException {
+        setNameFromCommand(param, task);
+        setTagsFromCommand(param, task);
+        setLevelFromCommand(param, task);
+        setNoteFromCommand(param, task);
+        setConditionalDatesFromCommand(param, task);
+    }
+
+    static void modifyDeadlineTask(
+            Hashtable<ParamEnum, ArrayList<String>> param, Task task)
+                    throws InvalidDateFormatException, InvalidInputException {
+        setNameFromCommand(param, task);
+        setTagsFromCommand(param, task);
+        setLevelFromCommand(param, task);
+        setNoteFromCommand(param, task);
+        setDueDateFromCommand(param, task);
+    }
+
+    static void modifyFloatingTask(
+            Hashtable<ParamEnum, ArrayList<String>> param, Task task)
+                    throws InvalidDateFormatException, InvalidInputException {
+        setNameFromCommand(param, task);
+        setTagsFromCommand(param, task);
+        setLevelFromCommand(param, task);
+        setNoteFromCommand(param, task);
+    }
+
+    static void modifyTimedTask(Hashtable<ParamEnum, ArrayList<String>> param,
+            Task task) throws InvalidDateFormatException, InvalidInputException {
+        setNameFromCommand(param, task);
+        setTagsFromCommand(param, task);
+        setLevelFromCommand(param, task);
+        setNoteFromCommand(param, task);
+        setStartDateFromCommand(param, task);
+        setEndDateFromCommand(param, task);
+        checkStartDateIsBeforeEndDate(task.getDateStart(), task.getDateEnd());
     }
 
     static void uncompleteTask(Task task) {
@@ -111,6 +100,11 @@ public class TaskModifier {
 
     }
 
+    static void undeleteTask(Task task) {
+        assert (task.isDeleted());
+        task.setDeleted(false);
+    }
+
     private static void checkStartDateIsBeforeEndDate(Calendar dateStart,
             Calendar dateEnd) throws InvalidInputException {
         if (!isStartDateBeforeEndDate(dateStart, dateEnd)) {
@@ -118,14 +112,59 @@ public class TaskModifier {
         }
     }
 
+    private static PriorityLevelEnum getPriorityEnumAsInteger(String levelString)
+            throws InvalidInputException {
+        try {
+            return parsePriorityAsInteger(levelString);
+        } catch (InvalidPriorityLevelException | NumberFormatException e1) {
+            throw new InvalidInputException(MessageCreator.createMessage(
+                    INVALID_PRIORITY_LEVEL_MESSAGE, levelString, null));
+        }
+    }
+
+    private static PriorityLevelEnum getPriorityEnumAsString(String levelString)
+            throws InvalidPriorityLevelException {
+        return PriorityLevelEnum.fromString(levelString);
+    }
+
+    private static PriorityLevelEnum getPriorityLevel(String levelString)
+            throws InvalidInputException {
+        PriorityLevelEnum priorityEnum;
+        try {
+            priorityEnum = getPriorityEnumAsString(levelString);
+        } catch (InvalidPriorityLevelException e) {
+            priorityEnum = getPriorityEnumAsInteger(levelString);
+        }
+        return priorityEnum;
+    }
+
+    private static boolean hasNullConditionalDates(Task task) {
+        return task.getConditionalDates() == null;
+    }
+
+    private static boolean isIdOutsideConditionalDatesRange(int dateId,
+            Task task) {
+        return task.getConditionalDates().size() <= dateId;
+    }
+
+    private static boolean isLessThanMinId(int dateId) {
+        return dateId < MIN_ID;
+    }
+
     private static boolean isStartDateBeforeEndDate(Calendar dateStart,
             Calendar dateEnd) {
         return dateStart.getTimeInMillis() < dateEnd.getTimeInMillis();
     }
 
+    private static PriorityLevelEnum parsePriorityAsInteger(String levelString)
+            throws InvalidInputException, InvalidPriorityLevelException {
+        int level = Integer.parseInt(levelString);
+        return PriorityLevelEnum.fromInteger(level);
+    }
+
     private static void setConditionalDatesFromCommand(
             Hashtable<ParamEnum, ArrayList<String>> param, Task task)
-            throws InvalidDateFormatException, InvalidInputException {
+                    throws InvalidDateFormatException, InvalidInputException {
         if (param.containsKey(ParamEnum.START_DATE)
                 && param.containsKey(ParamEnum.END_DATE)) {
             ArrayList<String> startDates = param.get(ParamEnum.START_DATE);
@@ -145,17 +184,9 @@ public class TaskModifier {
         }
     }
 
-    private static void setNameFromCommand(
-            Hashtable<ParamEnum, ArrayList<String>> param, Task task) {
-        if (param.containsKey(ParamEnum.NAME)) {
-            String taskName = param.get(ParamEnum.NAME).get(0);
-            task.setName(taskName);
-        }
-    }
-
     private static void setDueDateFromCommand(
             Hashtable<ParamEnum, ArrayList<String>> param, Task task)
-            throws InvalidDateFormatException {
+                    throws InvalidDateFormatException {
         if (param.containsKey(ParamEnum.DUE_DATE)
                 && !param.get(ParamEnum.DUE_DATE).get(0).isEmpty()) {
             Calendar dueDate = DateParser.parseString(param.get(
@@ -166,7 +197,7 @@ public class TaskModifier {
 
     private static void setEndDateFromCommand(
             Hashtable<ParamEnum, ArrayList<String>> param, Task task)
-            throws InvalidDateFormatException {
+                    throws InvalidDateFormatException {
         if (param.containsKey(ParamEnum.END_DATE)
                 && !param.get(ParamEnum.END_DATE).get(0).isEmpty()) {
             Calendar endDate = DateParser.parseString(param.get(
@@ -176,20 +207,9 @@ public class TaskModifier {
 
     }
 
-    private static void setStartDateFromCommand(
-            Hashtable<ParamEnum, ArrayList<String>> param, Task task)
-            throws InvalidDateFormatException {
-        if (param.containsKey(ParamEnum.START_DATE)
-                && !param.get(ParamEnum.START_DATE).get(0).isEmpty()) {
-            Calendar startDate = DateParser.parseString(param.get(
-                    ParamEnum.START_DATE).get(0));
-            task.setDateStart(startDate);
-        }
-    }
-
     private static void setLevelFromCommand(
             Hashtable<ParamEnum, ArrayList<String>> param, Task task)
-            throws InvalidInputException {
+                    throws InvalidInputException {
         if (param.containsKey(ParamEnum.LEVEL)) {
             assert param.get(ParamEnum.LEVEL).size() == 1;
             String levelString = param.get(ParamEnum.LEVEL).get(0);
@@ -198,37 +218,12 @@ public class TaskModifier {
         }
     }
 
-    private static PriorityLevelEnum getPriorityLevel(String levelString)
-            throws InvalidInputException {
-        PriorityLevelEnum priorityEnum;
-        try {
-            priorityEnum = getPriorityEnumAsString(levelString);
-        } catch (InvalidPriorityLevelException e) {
-            priorityEnum = getPriorityEnumAsInteger(levelString);
+    private static void setNameFromCommand(
+            Hashtable<ParamEnum, ArrayList<String>> param, Task task) {
+        if (param.containsKey(ParamEnum.NAME)) {
+            String taskName = param.get(ParamEnum.NAME).get(0);
+            task.setName(taskName);
         }
-        return priorityEnum;
-    }
-
-    private static PriorityLevelEnum getPriorityEnumAsInteger(String levelString)
-            throws InvalidInputException {
-        try {
-            return parsePriorityAsInteger(levelString);
-        } catch (InvalidPriorityLevelException | NumberFormatException e1) {
-            throw new InvalidInputException(MessageCreator.createMessage(
-                    INVALID_PRIORITY_LEVEL_MESSAGE, levelString, null));
-        }
-    }
-
-    private static PriorityLevelEnum getPriorityEnumAsString(String levelString)
-            throws InvalidPriorityLevelException {
-        return PriorityLevelEnum.fromString(levelString);
-    }
-
-    private static PriorityLevelEnum parsePriorityAsInteger(
-            String levelString) throws InvalidInputException,
-            InvalidPriorityLevelException {
-        int level = Integer.parseInt(levelString);
-        return PriorityLevelEnum.fromInteger(level);
     }
 
     private static void setNoteFromCommand(
@@ -236,6 +231,17 @@ public class TaskModifier {
         if (param.containsKey(ParamEnum.NOTE)) {
             String note = param.get(ParamEnum.NOTE).get(0);
             task.setNote(note);
+        }
+    }
+
+    private static void setStartDateFromCommand(
+            Hashtable<ParamEnum, ArrayList<String>> param, Task task)
+                    throws InvalidDateFormatException {
+        if (param.containsKey(ParamEnum.START_DATE)
+                && !param.get(ParamEnum.START_DATE).get(0).isEmpty()) {
+            Calendar startDate = DateParser.parseString(param.get(
+                    ParamEnum.START_DATE).get(0));
+            task.setDateStart(startDate);
         }
     }
 
@@ -247,16 +253,13 @@ public class TaskModifier {
         }
     }
 
-    private static boolean isIdOutsideConditionalDatesRange(int dateId,
-            Task task) {
-        return task.getConditionalDates().size() <= dateId;
-    }
+    private static final String INVALID_PRIORITY_LEVEL_MESSAGE = "%1$s is not a valid priority level.";
 
-    private static boolean hasNullConditionalDates(Task task) {
-        return task.getConditionalDates() == null;
-    }
+    private static final String INVALID_CONDITIONAL_DATE_ID_MESSAGE = "The conditional date id is invalid.";
 
-    private static boolean isLessThanMinId(int dateId) {
-        return dateId < MIN_ID;
-    }
+    private static final int MIN_ID = 0;
+
+    private static final String INVALID_CONFIRMED_TASK_MESSAGE = "The task is already confirmed.";
+
+    private static final String INVALID_START_END_DATE_MESSAGE = "The start date should occur before the end date";
 }
