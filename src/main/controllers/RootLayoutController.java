d70eb920c26a070722fa5b986358bae5daf89067
Sue Zheng Hao <szhlibrary@gmail.com>
2014-10-31 23:32:11 +0800

    Removed unused import.

--- src/main/controllers/RootLayoutController.java
+++ src/main/controllers/RootLayoutController.java
@@ -3,7 +3,6 @@ package main.controllers;
 import command.CommandParser;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
