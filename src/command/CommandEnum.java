b24d10ac086aa5e20adba09e7652b70cd9d12585
Pan Peining <peining_pan@hotmail.com>
2014-11-05 16:02:51 +0800

    More formatting

--- src/command/CommandEnum.java
+++ src/command/CommandEnum.java
@@ -9,21 +9,24 @@ package command;
  */
 public enum CommandEnum {
     ADD("add", ParamEnum.NAME, ParamEnum.DUE_DATE, ParamEnum.OR_END,
-            ParamEnum.OR_FROM, ParamEnum.DATE, ParamEnum.START_DATE,
-            ParamEnum.END_DATE, ParamEnum.LEVEL, ParamEnum.NOTE, ParamEnum.TAG), DELETE(
-            "delete", ParamEnum.KEYWORD), UPDATE("update", ParamEnum.KEYWORD,
-                    ParamEnum.DUE_DATE, ParamEnum.OR_END, ParamEnum.OR_FROM,
-                    ParamEnum.DATE, ParamEnum.START_DATE, ParamEnum.END_DATE,
-                    ParamEnum.LEVEL, ParamEnum.NOTE, ParamEnum.TAG, ParamEnum.NAME), UNDO(
-            "undo", ParamEnum.KEYWORD), SEARCH("search", ParamEnum.KEYWORD,
-            ParamEnum.NAME, ParamEnum.NOTE, ParamEnum.TAG, ParamEnum.STATUS,
-                            ParamEnum.AFTER, ParamEnum.BEFORE, ParamEnum.START_DATE,
-                            ParamEnum.END_DATE, ParamEnum.ON), DISPLAY("display",
-            ParamEnum.KEYWORD), DONE("done", ParamEnum.KEYWORD, ParamEnum.DATE), COMPLETE(
-            "complete", ParamEnum.KEYWORD, ParamEnum.DATE), TAG("\\+",
-            ParamEnum.KEYWORD), LEVEL("level", ParamEnum.KEYWORD), CONFIRM(
-            "confirm", ParamEnum.KEYWORD, ParamEnum.ID), CLEAR("clear",
-            ParamEnum.KEYWORD), TAB("tab", ParamEnum.KEYWORD);
+        ParamEnum.OR_FROM, ParamEnum.DATE, ParamEnum.START_DATE,
+        ParamEnum.END_DATE, ParamEnum.LEVEL, ParamEnum.NOTE, ParamEnum.TAG),
+    DELETE("delete", ParamEnum.KEYWORD),
+    UPDATE("update", ParamEnum.KEYWORD, ParamEnum.DUE_DATE, ParamEnum.OR_END,
+        ParamEnum.OR_FROM, ParamEnum.DATE, ParamEnum.START_DATE,
+        ParamEnum.END_DATE,ParamEnum.LEVEL, ParamEnum.NOTE, ParamEnum.TAG, ParamEnum.NAME),
+    UNDO("undo", ParamEnum.KEYWORD),
+    SEARCH("search", ParamEnum.KEYWORD, ParamEnum.NAME, ParamEnum.NOTE,
+        ParamEnum.TAG, ParamEnum.STATUS, ParamEnum.AFTER, ParamEnum.BEFORE,
+        ParamEnum.START_DATE, ParamEnum.END_DATE, ParamEnum.ON),
+    DISPLAY("display", ParamEnum.KEYWORD),
+    DONE("done", ParamEnum.KEYWORD, ParamEnum.DATE),
+    COMPLETE("complete", ParamEnum.KEYWORD, ParamEnum.DATE),
+    TAG("\\+", ParamEnum.KEYWORD),
+    LEVEL("level", ParamEnum.KEYWORD),
+    CONFIRM("confirm", ParamEnum.KEYWORD, ParamEnum.ID),
+    CLEAR("clear", ParamEnum.KEYWORD),
+    TAB("tab", ParamEnum.KEYWORD);
 
     private final String regex;
     private final ParamEnum commandKey;
